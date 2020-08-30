a973fab9a1f5f54c842c3595bea81deb80eec96f
Bruno <bruno@tzadi.com>
2020-08-30 19:48:15 -0300

    using class to create components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [{ name: "Bruno da Silva João", age: 10 }],
+  };
+
+  switchNameHandler = () => {
+    this.setState({ persons: [{ name: "se da crapa", age: 20 }] });
+    console.log("AQUI");
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Presentation App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person age={this.state.persons[0].age} />
       </div>
     );
   }
